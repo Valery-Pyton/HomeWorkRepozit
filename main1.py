df59631a4c0304e43b45a1e250c1a5fbfc1c9a2f
Valery-Pyton <nuclearsister@yandex.ru>
2024-08-02 23:44:59 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,14 @@
+print("hello world" )
+ #1st program
+print('(9**0.5)*5')
+print(9**0.5*5)
+#2nd program
+print(99.9 > 99.8 and 1000 != 1000.1)
+#3rd program
+print(1234//10%100 + 5678//10%100)
+#4th program
+print(13.42 !=42.13)
+print(13.42==42.13 or 13==42 or 13==13)
+print(13.42==42.13 or 42==13 or 42==42)
+
+
