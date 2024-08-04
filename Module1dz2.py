961768ac34c18cc58eae6f70c1507138f142220f
Valery-Pyton <nuclearsister@yandex.ru>
2024-08-05 01:45:53 +0300

    Add files via upload

---
+++ Module1dz2.py
@@ -0,0 +1,9 @@
+
+print("Курс:" ,subject_name ,",всего задач:" ,first_name ,",затрачено часов:" ,second_name ,",среднее время выполнения"
+      ,second_nam e /first_name ,"часа.")
+first_nam e =12
+second_nam e =1.5
+subject_nam e ="Py
+ton"
+timing=0.125
+
