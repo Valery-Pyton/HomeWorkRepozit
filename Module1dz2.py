0e7627f237e742a7b3ad0c7a15412192bb19196f
Valery-Pyton <nuclearsister@yandex.ru>
2024-08-05 01:53:20 +0300

    Update Module1dz2.py

--- Module1dz2.py
+++ Module1dz2.py
@@ -1,9 +1,5 @@
-
-print("Курс:" ,subject_name ,",всего задач:" ,first_name ,",затрачено часов:" ,second_name ,",среднее время выполнения"
-      ,second_nam e /first_name ,"часа.")
-first_nam e =12
-second_nam e =1.5
-subject_nam e ="Py
-ton"
+first_name =12
+second_name =1.5
+subject_name ="Pyton"
 timing=0.125
-
+print("Курс:" ,subject_name,",всего задач:" ,first_name ,",затрачено часов:" ,second_name ,",среднее время выполнения",second_name /first_name ,"часа.")
