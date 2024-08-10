5d5cd5e08100f40964d0190143d32f6a3e51d0e3
Valery-Pyton <nuclearsister@yandex.ru>
2024-08-11 00:05:06 +0300

    Update 'module_1_3'2.py

--- 'module_1_3'.py
+++ 'module_1_3'.py
@@ -2,9 +2,10 @@ name='Valery'
 print(name,type(name))
 age=32
 print(age,type(age))
-new_age=1
-print(age+new_age)
+age=(age+1)
+print(age+1,type(age+1))
 name='is_student'
+print('is_student'and'is_student')
 print((name)==(name))
 print(name,type(name))
 
