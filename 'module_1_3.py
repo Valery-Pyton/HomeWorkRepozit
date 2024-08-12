06c6b0a4dcf8886cadc7e5a72837af4302bfc984
Valery-Pyton <nuclearsister@yandex.ru>
2024-08-13 01:15:30 +0300

    Add files via upload

---
+++ 'module_1_3.py
@@ -0,0 +1,10 @@
+name='Valery'
+print(name,type(name))
+age=32
+print(age,type(age))
+age=(age+1)
+print(age+1,type(age+1))
+print('is_sudent'and 'is_student')
+print('is_student = True')
+print((name)==(name))
+print(name,type(name))
