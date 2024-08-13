ed11cae092d5da0f890103af6d9798ba97b554fa
Valery-Pyton <nuclearsister@yandex.ru>
2024-08-14 02:27:43 +0300

    Add files via upload

---
+++ 'module_1_7'.py
@@ -0,0 +1,17 @@
+students = {'Aaron','Bilbo','Johhny','Khendrick','Steve'}
+grades = [[5, 5, 5, 4, 5],[2, 2, 2, 3],[5, 3, 3, 5, 4],[4, 4, 3],[4, 5, 5, 2]]
+students=sorted(students)
+
+a = (sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]),
+     sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4]))
+
+klass = {students[0]: a[0], students[1]: a[1], students[2]: a[2],
+         students[3]: a[3], students[4]: a[4]}
+
+print(klass)
+
+
+
+
+
+
