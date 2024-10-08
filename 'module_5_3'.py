07fe38211391b69d0bfc0d593da199505e57cf21
Valery-Pyton <nuclearsister@yandex.ru>
2024-10-09 01:17:26 +0300

    Update 'module_5_3'2.py

--- 'module_5_3'.py
+++ 'module_5_3'.py
@@ -7,53 +7,108 @@ class House:
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-def __eq__(self,other):
-    return self.number_of_floors==other.number_of_floors
-print(h1==h2) #  возвращает False
+def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
 
+def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
 
-def __lt__(self, other): # (<)
-    return self.nFloors < other.nFloors
+def __len__(self):
+        return self.number_of_floors
 
- def __le__(self, other): #(<=)
-        return self.nFloors <= other.nFloors
+def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
 
-def __gt__(self, other): #(>)
-        return self.nFloors > other.nFloors
-
-def __ge__(self, other): #(>=)
-        return self.nFloors >= other.nFloors
-
-def __ne__(self, other): #(!=)
-        return self.nFloors != other.nFloors
-
-def __add__(self, value):
-        self.nFloors += value
+def __add__(self, other):
+        if isinstance(other, House):
+            self.number_of_floors += other.number_of_floors
+        elif isinstance(other, int):
+            self.number_of_floors += other
         return self
 
-def __iadd__(self, other):
-        return self + other
-
 def __radd__(self, other):
-        return self + other
+       return self.__add__(other)
+
+def __iadd__(self, other):
+        return self.__add__(other)
+
+def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+
+def __ge__(self, other):
+        return not self.__lt__(other)
+
+def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
+
+def __gt__(self, other):
+        return self.__le__(other)
+
+def __ne__(self, other):
+        return not self.__eq__(other)
+
+def go_to(self, new_floor):
+        list_ = []
+        for i in range(1, self.number_of_floors + 1):
+            list_.append(i)
+        if new_floor in list_:
+            for j in range(1, new_floor + 1):
+                if j != new_floor:
+                    print(j)
+                else:
+                    print(f'Приехали на {new_floor} этаж {self.name}')
+                    print()
+        else:
+            print(f'В {self.name},такого этажа не существует')
 
 
-def main():
-    h1 = House('ЖК Эльбрус', 10)
-    h2 = House('ЖК Акация', 20)
+dom1 = House('ЖК Эльбрус', 30)
+dom2 = House('ЖК Уют', 10)
 
-    print(h1)
-    print(h2)
+House.go_to(dom1, 16)
+House.go_to(dom2, 9)
 
-    print(h1 == h2)  # __eq__
+print()
+print(f'{dom1.name}, количество этажей: {dom1.number_of_floors}')
+print(f'{dom2.name}, количество этажей: {dom2.number_of_floors}')
+print()
+print('Магический метод __str__:')
+print(dom1)
+print(dom2)
+print()
+print('Магический метод __len__:')
+print(len(dom1))
+print(len(dom2))
+print()
+print('Метод "eq":', dom1 == dom2)
+print()
+print('Метод "add":', dom1 + 10)
+print(dom1 == dom2)
+print()
+dom1 += 10
+print('Метод "iadd":', dom1)
+print()
+print('Метод "radd":', 10 + dom2)
+print()
+print('Метод "gt":', dom1 > dom2)
+print()
+print('Метод "ge":', dom1 >= dom2)
+print()
+print('Метод "lt":', dom1 < dom2)
+print()
+print('Метод "le":', dom1 <= dom2)
+print()
+print('Метод "ne":', dom1 != dom2)
 
-    h1 = h1 + 10  # __add__
-    print(h1)
-    print(h1 == h2)
 
-    h1 += 10  # __iadd__
-    print(h1)
-
-    h2 = 10 + h2  # __radd__
-    print(h2)
 
